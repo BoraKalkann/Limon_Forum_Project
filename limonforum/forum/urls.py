92513a32dfafa6d19f0d5e4d87db69044627aafc
Bora KALKAN <borakalkan350@gmail.com>
2025-05-21 15:31:46 +0300

    PLS-59 Made all API calls for categories on the content detail pages

--- limonforum/forum/urls.py
+++ limonforum/forum/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('category', views.categoryPage, name='categoryPage'),
     path('contact', views.contactPage, name='contactPage'),
     path('search-result', views.searchResultPage, name='searchResultPage'),
-    path('single/<slug:slug>/', views.singlePage, name='singlePage'),
+    path('single/game/<slug:slug>/', views.singleGamePage, name='singleGamePage'),
+    path('single/movie/<slug:slug>/', views.singleMoviePage, name='singleMoviePage'),
+    path('single/series/<slug:slug>/', views.singleSeriesPage, name='singleSeriesPage'),
+    path('single/book/<slug:slug>/', views.singleBookPage, name='singleBookPage'),
     path('login/', views.login_view, name='loginPage'),
     path('register/', views.register_view, name='registerPage'),
 ]
