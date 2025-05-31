8b6308148cccae107cca6457bddf86492602237b
miraç özmen <miracozmen9@gmail.com>
2025-05-31 22:28:58 +0300

    PLS-62 Comment model created for game view.

--- limonforum/forum/models.py
+++ limonforum/forum/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class GameComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game_slug = models.SlugField()  
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.content[:30]}"
+
+
